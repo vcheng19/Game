6972ae90128cffbe092f5514a941a32969194e62
Virginia Cheng <vc54@duke.edu>
2016-01-26 22:39:37 -0500

    Update bossLevel.java

--- src/game_vc54/bossLevel.java
+++ src/game_vc54/bossLevel.java
@@ -2,10 +2,10 @@
 // Virginia Cheng
 // The purpose of this class is to take care of the final level and the boss pizza.
 // I think it is well designed because it is a simple class that only deals with one level.
-// I originally had this in one class, so I took it out of the first class so it would be easier to understand and read.
-// I also changed some variable names so they were more descriptive and I changed the methods so they were formatted verb then noun.
+// I originally had only one class for this project, so I decided to make this a seperate class in order to make it easier to read and understand.
+// I also changed some variable names so they were more descriptive and changed the method names so they were consistently formatted with a verb first and then a noun.
 // Like in my original code, I believe there is no duplicated code and no magic numbers.
-// The design of this class is pretty simple. It sets up the boss level, tracks how many lives the boss currently has, and determines when and where the boss moves.
+// The design of this class is pretty simple. It sets up the boss level, tracks how many lives the boss currently has, and determines when and where the boss moves, along with allowing the programmer to input the number of lives and the speed of the boss which makes the class more usable and versatile. 
 
 package game_vc54;
 
@@ -15,21 +15,21 @@ import javafx.scene.image.ImageView;
 public class bossLevel{
 	private int maxLives;
 	private int bossLives = 0;
-	private int moveTime;
+	private int moveSpeed;
 	private int timeCounter = 0;
 	private ImageView bossPizza;
 	double size;
 	
 	public bossLevel(int numLives, int numTime, ImageView bossImage){
 		maxLives = numLives;
-		moveTime = numTime;
+		moveSpeed = numTime;
 		
     		bossPizza = bossImage;
 	}
 	
 	public void moveBoss(){
 	 	timeCounter++;
-    		if(timeCounter > moveTime){
+    		if(timeCounter > moveSpeed){
     			setCoordinates(bossPizza);
     			timeCounter = 0;
     		}
